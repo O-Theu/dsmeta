936d98646a69cf6fde4486fb9c033ae9669a8aae
Mateus <omateus.conceicao@gmail.com>
2022-11-18 16:39:20 -0300

    Fix react

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,27 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
 import iconNotification from '../../assets/img/iconNotification.svg';
+import { BASE_URL } from '../../utils/request';
 
 import './styles.css';
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+const handleClick = (id : number) => {
+    axios.get(`${BASE_URL}/sales/${id}/notification`)
+        .then(resp => {
+            toast.success("SMS enviado com sucesso!")
+        })
+        .catch(err => {
+            toast.error("Algo deu errado!")
+        })
+}
+
+function NotificationButton( {saleId} : Props) {
     return(
-        <div className='dsmeta-red-btn'>
+        <div className='dsmeta-red-btn' onClick={() => handleClick(saleId)}> 
             <img src={iconNotification} alt='icone para enviar notificação'/>
         </div>
     )
